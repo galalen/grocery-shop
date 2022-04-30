fc116daacfbe0de92ffe0d9c4509917e35b151cd
Mohammed Galalen <mohammedgalalen@gmail.com>
2022-04-30 17:40:52 +0400

    add search products api

--- blueprints/product.py
+++ blueprints/product.py
@@ -17,7 +17,6 @@ def upload_products():
     """
     Upload product to the database
     """
-    user_id = get_jwt_identity()
     upload_file = request.files['file']
     if not upload_file:
         return {'error': 'No file selected'}, 400
@@ -32,15 +31,18 @@ def upload_products():
         rows.append(dict(zip(header, row)))
 
     try:
+        product_schema = ProductSchema()
+
         # cast data before saving to database
         for row in rows:
             row['price'] = float(row['price'])
             row['barcode'] = int(row['barcode'])
             row['available'] = row['available'] == 'TRUE'
+            product_schema.load(row)
 
         db.products.insert_many(rows)
-    except Exception:
-        return {'error': 'Verify file format'}, 400
+    except Exception as e:
+        return {'error': 'Check file content and format'}, 400
 
     return {"message": "Products uploaded successfully"}, 201
 
@@ -68,3 +70,45 @@ def add_review():
     review_data['createdAt'] = datetime.now()
     db.reviews.insert_one(review_data)
     return {'message': 'Review added successfully'}, 201
+
+
+@product_api.route('/search', methods=['POST'])
+def search_products():
+    """
+    Search for a product
+    """
+    query = request.args.get('searchText')
+    page = int(request.args.get('page', 0))
+    limit = 10
+
+    if query:
+        count = db.products.count_documents({'$text': {'$search': query}})
+        pipline = [
+            {"$match": {"$text": {'$search': query}}},
+            {"$lookup": {"from": "reviews", "localField": "barcode", "foreignField": "barcode", "as": "reviews"}},
+            {"$sort": {"name": 1}},
+            {"$skip": page * limit},
+            {"$limit": limit}
+        ]
+        products = db.products.aggregate(pipline)
+    else:
+        count = db.products.count_documents({})
+        pipline = [
+            {"$lookup": {"from": "reviews", "localField": "barcode", "foreignField": "barcode", "as": "reviews"}},
+            {"$sort": {"name": 1}},
+            {"$skip": page * limit},
+            {"$limit": limit}
+        ]
+        products = db.products.aggregate(pipline)
+
+    # fetch reviews names
+    result = []
+    for product in products:
+        for review in product['reviews']:
+            user = db.users.find_one({'_id': review['userId']})
+            review['name'] = user['name']
+        result.append(product)
+
+    products_schema = ProductSchema(many=True)
+    data = products_schema.dump(result)
+    return {"totalCount": count, "products": data}, 200
--- schemas/product.py
+++ schemas/product.py
@@ -2,9 +2,24 @@ from marshmallow import Schema, fields
 from schemas.fields import ObjectId
 
 
+class ReviewSchema(Schema):
+
+    _id = ObjectId(load_only=True)
+    userId = ObjectId(required=True)
+    barcode = fields.Int(required=True)
+    review = fields.Str(required=True)
+    createdAt = fields.DateTime(dump_only=True)
+
+
+class NestedReviewSchema(Schema):
+
+    name = fields.Str(required=True)
+    review = fields.Str(required=True)
+
+
 class ProductSchema(Schema):
 
-    _id = ObjectId(dump_only=True)
+    _id = ObjectId(load_only=True)
     name = fields.Str(required=True)
     barcode = fields.Int(required=True)
     brand = fields.Str(required=True)
@@ -12,11 +27,4 @@ class ProductSchema(Schema):
     price = fields.Float(required=True)
     available = fields.Boolean(required=True)
 
-
-class ReviewSchema(Schema):
-
-    _id = ObjectId(dump_only=True)
-    userId = ObjectId(required=True)
-    barcode = fields.Int(required=True)
-    review = fields.Str(required=True)
-    createdAt = fields.DateTime(dump_only=True)
+    reviews = fields.Nested(NestedReviewSchema, many=True, dump_only=True, required=False)
